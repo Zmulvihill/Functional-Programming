cb1daaebf041dd68aa1a266bf6936726faa96881
Zmulvihill <zmulvihi@yahoo.com>
2023-03-22 10:32:14 -0700

    added script20 and ran node

---
+++ script21.js
@@ -0,0 +1,13 @@
+//Apply Functional Programming to Convert Strings to URL Slugs
+// Only change code below this line
+function urlSlug(title) {
+    return title
+      .toLowerCase()
+      .trim()
+      .split(/\s+/)
+      .join("-");
+  }
+  // Only change code above this line
+  console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"))
+  console.log(urlSlug("Winter Is Coming"))
+  console.log(urlSlug("Hold The Door"))
