dddb137f25c2d3b1e3a917589e8f19f555dd9b95
Zmulvihill <zmulvihi@yahoo.com>
2023-03-22 08:38:35 -0700

    added script19 and ran node

---
+++ script19.js
@@ -0,0 +1,11 @@
+//Split a String into an Array Using the split Method
+function splitify(str) {
+    // Only change code below this line
+  return str.split(/\W/);
+  
+    // Only change code above this line
+  }
+  
+  console.log(splitify("Hello World,I-am code"))
+  console.log(splitify("Earth-is-our home"))
+  console.log(splitify("This.is.a-sentence"))
