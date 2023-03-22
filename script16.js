a5561bad705388317246e14a4bc99a619b14ad03
Zmulvihill <zmulvihi@yahoo.com>
2023-03-22 07:40:29 -0700

    added script16 and ran node

---
+++ script16.js
@@ -0,0 +1,13 @@
+//Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+const squareList = arr => {
+    // Only change code below this line
+   return arr
+    .filter(num => num > 0 && num % parseInt(num) === 0)
+    .map(num => Math.pow(num, 2))
+    // Only change code above this line
+  };
+  
+  const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+  console.log(squaredIntegers);
+  console.log(squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]))
+  console.log(squareList([-3.7, -5, 3, 10, 12.5, 7, -4.5, -17, 0.3]))
