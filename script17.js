c0fb60ad3420d40b5332a951aae637ad3bde54b9
Zmulvihill <zmulvihi@yahoo.com>
2023-03-22 07:49:28 -0700

    added script17 and ran node

---
+++ script17.js
@@ -0,0 +1,12 @@
+//Sort an Array Alphabetically using the sort Method
+function alphabeticalOrder(arr) {
+    // Only change code below this line
+    return arr.sort(function(a, b) {
+      return a === b ? 0 : a < b ? -1 : 1;
+    })
+    // Only change code above this line
+  }
+  
+  console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]))
+  console.log(alphabeticalOrder(["x", "h", "a", "m", "n", "m"]))
+  console.log(alphabeticalOrder(["a", "a", "a", "a", "x", "t"]))
