53499a3060e727a77bb39dd2382766ea439b4f51
Zmulvihill <zmulvihi@yahoo.com>
2023-03-23 09:17:23 -0700

    added script22 and ran node

---
+++ script22.js
@@ -0,0 +1,11 @@
+//Use the some Method to Check that Any Elements in an Array Meet a Criteria
+function checkPositive(arr) {
+    // Only change code below this line
+  return arr.some(elem => elem > 0)
+  
+    // Only change code above this line
+  }
+  
+  console.log(checkPositive([1, 2, 3, -4, 5]))
+  console.log(checkPositive([1, 2, 3, 4, 5]))
+  console.log(checkPositive([-1, -2, -3, -4, -5]))
